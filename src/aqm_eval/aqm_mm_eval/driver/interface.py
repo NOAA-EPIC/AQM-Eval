00f67399f6129cf08d3754fa85c35fabf144d6ca
Ben Koziol <bkoziol@redlineperf.com>
2025-08-28 14:44:47 -0600

    add srw interface [ci skip]

--- src/aqm_eval/aqm_mm_eval/driver/interface.py
+++ src/aqm_eval/aqm_mm_eval/driver/interface.py
@@ -1,21 +1,23 @@
 import os
 from pathlib import Path
+from typing import Annotated
 
-from pydantic import BaseModel, computed_field
+from pydantic import BaseModel, computed_field, BeforeValidator
 
+PathExisting = Annotated[Path, BeforeValidator(lambda x: Path(x).exists())]
 
 class SRWInterface(BaseModel):
     model_config = {"frozen": True}
 
-    expt_dir: Path
+    expt_dir: PathExisting
 
     @computed_field
-    def config_path_user(self) -> Path:
+    def config_path_user(self) -> PathExisting:
         return self.expt_dir / "config.yaml"
 
     @computed_field
-    def config_path_rocoto(self) -> Path:
+    def config_path_rocoto(self) -> PathExisting:
         return self.expt_dir / "rocoto_defns.yaml"
 
-    def get_yaml_paths(self) -> tuple[Path, ...]:
+    def get_yaml_paths(self) -> tuple[PathExisting, ...]:
         return self.config_path_user, self.config_path_rocoto
