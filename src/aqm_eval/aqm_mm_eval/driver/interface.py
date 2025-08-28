4e4c3ca90b2a9bb2348fa7c65be76f3d15615d62
Ben Koziol <bkoziol@redlineperf.com>
2025-08-28 12:56:56 -0600

    added srw interface [ci skip]

---
+++ src/aqm_eval/aqm_mm_eval/driver/interface.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+
+from pydantic import BaseModel, computed_field
+
+
+class SRWInterface(BaseModel):
+    model_config = {"frozen": True}
+
+    expt_dir: Path
+
+    @computed_field
+    def config_path_user(self) -> Path:
+        return self.expt_dir / "config.yaml"
+
+    @computed_field
+    def config_path_rocoto(self) -> Path:
+        return self.expt_dir / "rocoto_defns.yaml"
