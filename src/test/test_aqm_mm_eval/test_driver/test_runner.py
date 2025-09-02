928b9bac7e942f140e758249c9cbeb6d64c8264b
Ben Koziol <bkoziol@redlineperf.com>
2025-09-02 13:45:17 -0600

    added test for control yaml files [ci skip]

--- src/test/test_aqm_mm_eval/test_driver/test_runner.py
+++ src/test/test_aqm_mm_eval/test_driver/test_runner.py
@@ -9,11 +9,27 @@ class TestMMEvalRunner:
 
         runner.initialize()
 
+        # Test links for all days are created
         actual_links = [ii for ii in srw_interface.link_alldays_path.iterdir()]
         # LOGGER(str(actual_links), level=logging.DEBUG)
         assert len(actual_links) == 50
 
-        assert (srw_interface.mm_run_dir / EvalType.CHEM.value / "namelist.yaml").exists()
+        # Test control yaml files exist as expected
+        chem_run_dir = srw_interface.mm_run_dir / EvalType.CHEM.value
+        actual_files = chem_run_dir.rglob("*")
+        assert set([ii.name for ii in actual_files]) == {
+            "control_spatial_bias.yaml",
+            "control_save_paired.yaml",
+            "control_stats.yaml",
+            "control_taylor.yaml",
+            "control_timeseries.yaml",
+            "control_csi.yaml",
+            "control_spatial_overlay.yaml",
+            "namelist.yaml",
+            "control_multi_boxplot.yaml",
+            "control_boxplot.yaml",
+        }
+
         tdk
 
         # runner.run(finalize=True)
